cf2c4227eabd4a33055579a115e6657b0305bd4c
SomeSimpleThings <makesomethingsimple@gmail.com>
2020-12-05 13:35:19 +0300

    initial

---
+++ src/test/kotlin/com/papuguys/camera/CameraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.papuguys.camera
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CameraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
