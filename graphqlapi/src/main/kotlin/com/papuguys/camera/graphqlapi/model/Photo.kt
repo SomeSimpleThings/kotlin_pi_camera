f350608a4b6ae0810f412a7385ab11a44e120fa1
SomeSimpleThings <makesomethingsimple@gmail.com>
2020-12-06 11:12:06 +0300

    photo service

--- graphqlapi/src/main/kotlin/com/papuguys/camera/graphqlapi/model/Photo.kt
+++ graphqlapi/src/main/kotlin/com/papuguys/camera/graphqlapi/model/Photo.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "photo")
-data class Photo(var name: String) {
+data class Photo(var name: String, var path: String) {
 
     @Id
     var id: String = ""
--- graphqlapi/src/main/kotlin/com/papuguys/camera/graphqlapi/resolver/PhotoResolver.kt
+++ graphqlapi/src/main/kotlin/com/papuguys/camera/graphqlapi/resolver/PhotoResolver.kt
@@ -3,44 +3,31 @@ package com.papuguys.camera.graphqlapi.resolver
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import com.papuguys.camera.graphqlapi.model.Photo
-import com.papuguys.camera.graphqlapi.repository.PhotoRepository
-import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class PhotoQueryResolver(
-    val photoRepository: PhotoRepository,
-    private val mongoOperations: MongoOperations
+    val photoService: PhotoService,
 ) : GraphQLQueryResolver {
 
     fun photos(): List<Photo> {
-        return photoRepository.findAll()
+        return photoService.findAll()
     }
 }
 
 
 @Component
-class PhotoMutationResolver(val photoRepository: PhotoRepository) : GraphQLMutationResolver {
+class PhotoMutationResolver(val photoService: PhotoService) : GraphQLMutationResolver {
 
     fun newPhoto(name: String): Photo {
-        val photo = Photo(name)
-        photo.id = UUID.randomUUID().toString();
-        photoRepository.save(photo)
-        return photo
+        return photoService.createPhoto(name)
     }
 
     fun updatePhoto(id: String, name: String): Photo {
-        val photo = photoRepository.findById(id)
-        photo.ifPresent {
-            it.name = name
-            photoRepository.save(it)
-        }
-        return photo.get()
+        return photoService.update(id, name)
     }
 
     fun deletePhoto(id: String): Boolean {
-        photoRepository.deleteById(id)
-        return true
+        return photoService.delete(id)
     }
 }
